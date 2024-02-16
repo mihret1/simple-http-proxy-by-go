e78c0a7a39581c5d784779f59898e26511c0a2fb
mihret1 <90724655+mihret1@users.noreply.github.com>
2024-02-16 23:32:01 +0300

    intialize the variable

--- main.go
+++ main.go
@@ -12,3 +12,13 @@ import (
 	"sync"
 	"time"
 )
+
+
+var backendQueue chan *Backend
+var requestBytes map[string]int64
+var requestLock sync.Mutex
+
+func init() {
+	requestBytes = make(map[string]int64)
+	backendQueue = make(chan *Backend, 10)
+}
